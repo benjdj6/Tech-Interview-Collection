90a35b723da24d36028bef6f5878b10feb0f7db2
benjdj6 <benjdj6@vt.edu>
2017-02-07 20:53:24 -0500

    Added more tests, and removed inefficient use of sublists for length

--- Interview Questions/shortTrips.py
+++ Interview Questions/shortTrips.py
@@ -8,9 +8,11 @@ def shortestTrip(A):
     needto = len(trip)
     start = 0
     loops = 0
+    length = 0
 
     for end in range(len(A)):
         loops += 1
+        length += 1
         if trip[A[end]] == 0:
             found += 1
             needto -= 1
@@ -18,15 +20,16 @@ def shortestTrip(A):
 
         while needto == 0:
             loops += 1
-            if len(A[start:end + 1]) < mini:
-                mini = len(A[start:end + 1])
+            if length < mini:
+                mini = length
             trip[A[start]] -= 1
             start += 1
+            length -= 1
             if trip[A[start - 1]] == 0:
                 found -= 1
                 needto += 1
                 break
-    # print loops, len(A) # keeping track of growth
+    # print loops, len(A)  # keeping track of growth
     return mini
 
 
@@ -34,3 +37,6 @@ print shortestTrip([1, 2, 3, 3, 4, 1, 1, 1, 1, 2, 3, 4, 1])
 print shortestTrip([1, 2, 3, 2, 4, 1])
 print shortestTrip([1, 4, 2, 3, 2, 1])
 print shortestTrip([1, 1, 1, 1, 1, 1])
+print shortestTrip([1] * 10000)
+print shortestTrip([1] * 1000)
+print shortestTrip([1] * 100)
